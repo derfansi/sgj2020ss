cd8c5136648caf27b3fa9046e68373517e4ab791
Kristin Fratzke <kristin.fratzke@tum.de>
2020-06-13 23:48:36 +0200

    Healthbar added

---
+++ Assets/scripts/Healthbar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+
+    public Slider healthbar;
+
+    public void SetHealth(int health)
+    {
+        healthbar.value = health;
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        healthbar.maxValue = health;
+    }
+}
