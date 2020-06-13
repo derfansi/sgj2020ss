5ac6028c4cb460fa5205ffc75efe06459b73a405
fansens <fansensGER@users.noreply.github.com>
2020-06-13 23:49:20 +0200

    added pressureplate + script

---
+++ Assets/Scenes/Scripts_Justin/PressurePlate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    
+        
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Rock"))
+        {
+            //TODO
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Rock"))
+        {
+            //TODO
+        }
+    }
+}
