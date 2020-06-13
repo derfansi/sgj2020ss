8bdf1d27fe7705e5585cccbcd3249e8ca6484d84
fansens <fansensGER@users.noreply.github.com>
2020-06-13 12:23:50 +0200

    added Reflect & Balance Enemies

--- Assets/Scenes/Scripts_Justin/Rod.cs
+++ Assets/Scenes/Scripts_Justin/Rod.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Reflect : MonoBehaviour
+public class Rod : MonoBehaviour
 {
-    private int _right = -1;
-    
+    private int _right = 1;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += Vector3.right * (_right * 0.04f);
+        transform.Rotate(Vector3.forward, _right * 0.2f);
     }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Reflect_Endpoint"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Rod_Endpoint"))
         {
             
             _right *= -1;
