5ead854055f07e12676da624abda078f7ef31ce0
Stefan W <ge57yuj@mytum.de>
2020-06-13 06:35:49 +0200

    started with MusicManager

---
+++ Assets/Stefans Shit/Scripts/MusicManagement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManagement : MonoBehaviour
+{
+    public AudioClip airMusic, waterMusic, earthMusic, fireMusic;
+
+    private AudioSource source;
+
+    public void BlendMusic(AudioClip newMusic)
+    {
+
+    }
+}
