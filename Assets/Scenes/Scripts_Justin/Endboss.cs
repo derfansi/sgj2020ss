26e0278bd548861937aaf0c7bbf68044f10562d3
fansens <fansensGER@users.noreply.github.com>
2020-06-14 11:41:08 +0200

    added endboss script

---
+++ Assets/Scenes/Scripts_Justin/Endboss.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Endboss : Enemy
+{
+    public override void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Fireball"))
+        {
+            Destroy(gameObject);
+            StartCoroutine(toCredits());
+        }
+    }
+
+    IEnumerator toCredits()
+    {
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(2);
+    }
+}
