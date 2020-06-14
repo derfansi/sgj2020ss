f1a815eff63ef498cabc36d21061993b2e0ab29f
Kristin Fratzke <kristin.fratzke@tum.de>
2020-06-14 09:49:09 +0200

    popup example

---
+++ Assets/scripts_jojo/popupText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popupText : MonoBehaviour
+{
+    public GameObject canvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //canvas.SetActive(true);
+            StartCoroutine(ExampleText());
+        }
+    }
+
+    IEnumerator ExampleText()
+    {
+        canvas.SetActive(true);
+        
+        yield return new WaitForSeconds(5);
+        
+        canvas.SetActive(false);
+        Destroy(canvas);
+        Destroy(gameObject);
+    }
+    /*private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(canvas);
+            Destroy(gameObject);
+        }
+    }*/
+}
