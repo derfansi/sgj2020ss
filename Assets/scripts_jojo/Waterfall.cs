5b8690ec0708aa3ffd86640532fb426b3a1378d4
fansens <fansensGER@users.noreply.github.com>
2020-06-14 04:53:19 +0200

    water is feddich

--- Assets/scripts_jojo/Waterfall.cs
+++ Assets/scripts_jojo/Waterfall.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Waterfall : Interactable
 {
-    public GameObject whip;
+    public GameObject wgc;
     public bool cmp = false;
-    public GameObject current;
+    private GameObject _current;
+    public GameObject wf;
+    private GameObject _waterFloor;
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<GameObject> _floors = new List<GameObject>();
+    private List<GameObject> _creatorsL = new List<GameObject>();
+    private Dictionary<GameObject, Coroutine> _creators = new Dictionary<GameObject, Coroutine>();
 
     private void Update()
     {
@@ -42,8 +42,13 @@ public class Waterfall : Interactable
     {
         if (cursorScript.element == MyCursor.Element.WATER)
         {
-            current = Instantiate(whip, cursor.transform.position, Quaternion.identity);
-            current.GetComponent<Whip>().Config(cursor, cursorScript);
+            _current = Instantiate(wgc);
+            _current.transform.parent = cursor.transform;
+            _current.transform.localPosition = Vector3.zero;
+            _creatorsL.Add(_current);
+            _creators[_current] = StartCoroutine(DestroyGroundCreator());
+            //current = Instantiate(whip, cursor.transform.position, Quaternion.identity);
+            //current.GetComponent<Whip>().Config(cursor, cursorScript);
         }
     }
 
@@ -51,8 +56,33 @@ public class Waterfall : Interactable
     {
         if (cursorScript.element == MyCursor.Element.WATER)
         {
-            current.GetComponent<Whip>().isHeld = false;
-            cmp = false;
+            //current.GetComponent<Whip>().isHeld = false;
+            StopCoroutine(_creators[_current]);
+            _creators.Remove(_current);
+            Vector2 pos = _current.transform.position;
+            
+            Destroy(_current);
+            _creatorsL.RemoveAt(0);
+            
+            _waterFloor = Instantiate(wf);
+            _waterFloor.transform.position = pos;
+            _floors.Add(_waterFloor);
+            StartCoroutine(DestroyGround());
         }
+    }
+
+    IEnumerator DestroyGroundCreator()
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(_creatorsL[0]);
+        _creatorsL.RemoveAt(0);
+        cmp = false;
+    }
+
+    IEnumerator DestroyGround()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Destroy(_floors[0]);
+        _floors.RemoveAt(0);
     }
 }
