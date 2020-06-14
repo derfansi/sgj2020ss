875c23056d089bbd696fd423baf43c32424960f7
Johannes Madest <johannesmadest@gmail.com>
2020-06-14 04:12:00 +0200

    wip wasser

---
+++ Assets/scripts_jojo/Waterfall.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waterfall : Interactable
+{
+    public GameObject whip;
+    public bool cmp = false;
+    public GameObject current;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            if (cursorScript.down && !cmp)
+            {
+                cmp = !cmp;
+                MouseDown();
+            }
+        }
+
+        if (activated)
+        {
+            if(!cursorScript.down && cmp)
+            {
+                cmp = !cmp;
+                MouseUp();
+            }
+        }
+    }
+
+
+    public override void MouseDown()
+    {
+        if (cursorScript.element == MyCursor.Element.WATER)
+        {
+            current = Instantiate(whip, cursor.transform.position, Quaternion.identity);
+            current.GetComponent<Whip>().Config(cursor, cursorScript);
+        }
+    }
+
+    public override void MouseUp()
+    {
+        if (cursorScript.element == MyCursor.Element.WATER)
+        {
+            current.GetComponent<Whip>().isHeld = false;
+            cmp = false;
+        }
+    }
+}
